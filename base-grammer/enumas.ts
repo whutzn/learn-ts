6c8b8e4ab479e71c708c6e39a0d6af88bee6de39
zengning <whutzn@foxmail.com>
2022-06-15 17:56:37 +0800

    feat(boring)

--- base-grammer/enumas.ts
+++ base-grammer/enumas.ts
@@ -3,3 +3,6 @@ let int2: number = 2;
 let binaryVar: number = 0b1010;
 let octalVar: number = 0o744;
 let hexVar: number = 0xf00d;
+
+let isMan: boolean = Boolean(1);
+let smxVar: string = String(1);
